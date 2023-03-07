c3863d63c578853f11414c7f2a594268a2270c1d
Hexamillion <48009181+geoffreyyyyy15@users.noreply.github.com>
2023-03-07 22:38:27 +0800

    code cleanup

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,14 +11,5 @@
 
     {{ $posts->links() }}
 </main>
-{{--
-            @foreach ($posts as $post)
-            <a href="/posts/{{$post->slug}}">  <h1 class="mx-5 m-3">{{ $post->title }}</h1></a>
-            <div class="border-b-2 border-sky-500 mx-5">
-         By <a href="/author/{{$post->author->username}}">{{ $post->author->name }}</a> in <a href="/categories/{{$post->category->slug}}" class="pt-5 mx-5 b-5">{{ $post->category->name }}</a>
-            <p class="pt-6 mx-5 b-5">{!! $post->body !!}</p>
-            </div>
-            @endforeach --}}
-
 
         </x-layout >
