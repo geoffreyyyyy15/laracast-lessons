7596dc79cf387ab78da42a94a9d4957f90a1f53c
Hexamillion <48009181+geoffreyyyyy15@users.noreply.github.com>
2023-03-08 17:51:17 +0800

    Routes for register, login and commenting

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostCommentsController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\SessionController;
 use App\Http\Controllers\TagController;
 use App\Http\Controllers\UserController;
 use App\Models\Category;
@@ -11,13 +13,22 @@ use Illuminate\Log\Logger;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-
+ 
 
 Route::get('/', [PostController::class, 'index']);
 Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+Route::post('posts/{post:slug}/comments', [PostCommentsController::class, 'store']);
 
-Route::get('/register', [RegisterController::class, 'create']);
-Route::post('/register', [RegisterController::class, 'store']);
+Route::get('/register', [RegisterController::class, 'create'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store'])->middleware('guest');
+
+
+
+Route::get('login', [SessionController::class, 'create'])->middleware('guest');
+Route::post('sessions', [SessionController::class, 'store'])->middleware('guest');
+
+
+Route::post('logout', [SessionController::class, 'destroy'])->middleware('auth');
 // Route::get('categories/{category:slug}', function (Category $category){
 //     return view('posts')
 //             ->with('posts', $category->posts)
