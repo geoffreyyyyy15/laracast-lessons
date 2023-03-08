d0f84273fddd8c6fa1fcd14ea53b311677b59fce
Hexamillion <48009181+geoffreyyyyy15@users.noreply.github.com>
2023-03-08 17:48:00 +0800

    create comment component

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,18 @@
+@props(['comment'])
+
+<x-panel>
+<article class="flex bg-gray-800 space-x-2">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/60?u={{$comment->user_id}}" class="rounded-xl"  alt="">
+    </div>
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">{{ $comment->author->username }}</h3>
+            <div class="text-xs">
+            <time><p>Posted</p>{{ $comment->created_at->format("F j, Y, g:i a")}}</time>
+        </div>
+        </header>
+        <p>{{ $comment->body }}</p>
+    </div>
+</article>
+</x-panel>
