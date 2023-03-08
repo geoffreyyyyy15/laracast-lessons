d766e1caf0d6ee6c015b5ebeae7840eeb7c7db82
Hexamillion <48009181+geoffreyyyyy15@users.noreply.github.com>
2023-03-08 17:49:20 +0800

    Adding welcoming authentication

--- resources/views/posts/__header.blade.php
+++ resources/views/posts/__header.blade.php
@@ -3,26 +3,17 @@
     <h1 class="text-4xl">
         Latest <span class="text-blue-500">Laravel From Scratch</span> News
     </h1>
+    @auth 
+    <h1 class="text-4xl">
+        Welcome Back! <span class="text-blue-500">{{ ucfirst(auth()->user()->name) }}</span> 
+    </h1>
+    @endauth
 
     <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-8">
         <!--  Category -->
         <div class="relative lg:inline-flex items-center bg-gray-100 rounded-xl">
            <x-category-dropdown />
         </div>
-
-        <!-- Other Filters -->
-        {{-- <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl">
-            <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
-                <option value="category" disabled selected>Other Filters
-                </option>
-                <option value="foo">Foo
-                </option>
-                <option value="bar">Bar
-                </option>
-            </select>
-          <x-down-arrow class="absolute pointer-events-none" />
-        </div> --}}
-
         <!-- Search -->
         <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
             <form method="GET" action="/">
