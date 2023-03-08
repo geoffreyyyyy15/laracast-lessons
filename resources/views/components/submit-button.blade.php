d94671de597b4de55ae4a4934112ddaf99a340d9
Hexamillion <48009181+geoffreyyyyy15@users.noreply.github.com>
2023-03-08 17:48:53 +0800

    clean up code adding attributes on button

---
+++ resources/views/components/submit-button.blade.php
@@ -0,0 +1,5 @@
+<div class="flex justify-end">
+    <button type="submit" class="rounded-2xl px-10 text-xs font-semibold py-2 uppercase text-black mt-2 bg-white">
+        {{$slot}}
+    </button>
+</div>
